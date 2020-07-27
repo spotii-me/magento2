ff5b4dd47672cf2aaa250ca95b5dbe7c58eee2f5
Alexis Haim <alexis@onvabosser.fr>
2020-07-27 11:51:12 +0400

    fix php error on class constant in redirect

--- Controller/Standard/Redirect.php
+++ Controller/Standard/Redirect.php
@@ -67,7 +67,8 @@ class Redirect extends SpotiiPay
         $quoteId = $quote->getId();
               // **
         $order = $this->_quoteManagement->submit($quote);
-        $reference = $payment->getAdditionalInformation(self::ADDITIONAL_INFORMATION_KEY_ORDERID);
+        $reference = $payment->getAdditionalInformation('spotii_order_id');
+        $this->spotiiHelper->logSpotiiActions("ORDER REF FROM REDIRECT : $reference");
         $this->_spotiipayModel->createTransaction($order, $reference, $quote);
         $quote->collectTotals()->save();          
         $order->setState("pending")->setStatus("pending");
