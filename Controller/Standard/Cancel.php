08ab9e26221b1d529c9da2ca5d9f72ac5689f4f5
Maimona <maimona.hajadbullah@gmail.com>
2020-08-26 12:26:31 +0400

    inventory

--- Controller/Standard/Cancel.php
+++ Controller/Standard/Cancel.php
@@ -15,18 +15,6 @@ use Spotii\Spotiipay\Controller\AbstractController\SpotiiPay;
  */
 class Cancel extends SpotiiPay
 {
-
-    public function increaseInventory($itemData) {
-        $sku = $itemData['sku'];
-        $qty = $itemData['quantity'];
-        $decrease= $qty-($qty*2);
-        $this->spotiiHelper->logSpotiiActions('sku ' . $sku .' Qty ' . $qty .' Decrease '.$decrease);
-        $stockItem = $this->stockRegistry->getStockItemBySku($sku);
-        $stockItem->setQty($decrease);
-        //$stockItem->setIsInStock((bool)$qty); // this line
-        $this->stockRegistry->updateStockItemBySku($sku, $stockItem);
-        $this->spotiiHelper->logSpotiiActions('result' . $this->stockRegistry->updateStockItemBySku($sku, $stockItem));
-    }
     /**
      * Cancel the order
      */
@@ -34,11 +22,15 @@ class Cancel extends SpotiiPay
     {
         $order = $this->getOrder();
         foreach ($order->getAllVisibleItems() as $item) {
-            $itemData = [
-                "sku" => $item->getSku(),
-                "quantity" => $item->getQtyOrdered(),
-            ];
-            increaseInventory($itemData);
+            $sku = $item->getSku();
+            $qty = $item->getQtyOrdered();
+            $decrease= $qty-($qty*2);
+            $this->spotiiHelper->logSpotiiActions('sku ' . $sku .' Qty ' . $qty .' Decrease '.$decrease);
+            $stockItem = $this->stockRegistry->getStockItemBySku($sku);
+            $stockItem->setQty($decrease);
+            //$stockItem->setIsInStock((bool)$qty); // this line
+            $this->stockRegistry->updateStockItemBySku($sku, $stockItem);
+            $this->spotiiHelper->logSpotiiActions('result' . $this->stockRegistry->updateStockItemBySku($sku, $stockItem));
         }
         $this->messageManager->addError("Spotiipay Transaction failed");
         $order->registerCancellation("Returned from Spotiipay without completing payment.");
@@ -49,7 +41,5 @@ class Cancel extends SpotiiPay
         $this->getResponse()->setRedirect(
             $this->_url->getUrl('checkout/onepage/failure')
        );
-       // $this->_redirect($redirect);
-       // http://{base_url}/sales/order/cancel/order_id/76/
     }
 }
