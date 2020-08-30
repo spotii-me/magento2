3c2a88ea369cba1dd36dba473220ab903dbecd2f
Maimona <maimona.hajadbullah@gmail.com>
2020-08-30 19:12:56 +0400

    not toOptionArray

--- Model/Config/Source/Order/OrderStatus.php
+++ Model/Config/Source/Order/OrderStatus.php
@@ -10,12 +10,12 @@ namespace Spotii\Spotiipay\Model\Config\Source\Order;
  * Class Mode
  * @package Spotii\Spotiipay\Model\Config\Source\Payment
  */
-class OrderStatus implements \Magento\Framework\Option\ArrayInterface
+class OrderStatus 
 {
     /**
      * @return array
      */
-    public function toOptionArray($statusCollectionFactory)
+    public function getStatus($statusCollectionFactory)
     {
         $options = $this->statusCollectionFactory->create()->toOptionArray();        
         return $options;
