b0911fef9a97fa158e29c6dde8b04a34bd70e523
Maimona <maimona.hajadbullah@gmail.com>
2020-09-14 12:51:30 +0400

    type

--- Model/Gateway/Transaction.php
+++ Model/Gateway/Transaction.php
@@ -104,13 +104,13 @@ class Transaction
         
         try {
                 $ordersCollection = $this->_orderCollectionFactory->create()
-                /*->join(
+                ->join(
                 'sales_order_address',
                 'sales_order_address.parent_id=main_table.entity_id',
                 'sales_order_address.country_id ') 
                 ->addFieldToFilter('country_id', 
                     array('IN' => 'AE')
-                )*/->addFieldToFilter(
+                )->addFieldToFilter(
                 'status',
                 ['eq' => $status]
                 )->addFieldToFilter(
@@ -120,7 +120,7 @@ class Transaction
                 'created_at',
                 ['lteq' => $today]
                 )->addAttributeToSelect('increment_id');
-            $this->spotiiHelper->logSpotiiActions("cron "+ sizeof($ordersCollection));
+             $this->spotiiHelper->logSpotiiActions("ordersCollection ".sizeof($ordersCollection));
             $body = $this->_buildOrderPayLoad($ordersCollection);
             $url = $this->spotiiApiConfig->getSpotiiBaseUrl() . '/v1.0/merchant' . '/magento/orders';
             $authToken = $this->config->getAuthToken();
