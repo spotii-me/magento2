aec9c6a9b25b880a7040c271d99093441f568c43
Maimona <maimona.hajadbullah@gmail.com>
2020-10-02 00:00:59 +0400

    decode fix

--- Controller/Standard/CheckInventory.php
+++ Controller/Standard/CheckInventory.php
@@ -21,7 +21,7 @@ class CheckInventory extends SpotiiPay
         $itemsString = $post['items'];
         $this->spotiiHelper->logSpotiiActions($itemsString);
 
-        $items = $this->jsonHelper->jsonDecode(stripslashes($itemsString));
+        $items = $this->jsonHelper->jsonDecode($itemsString);
         $this->spotiiHelper->logSpotiiActions($items);
 
         $flag = true;
