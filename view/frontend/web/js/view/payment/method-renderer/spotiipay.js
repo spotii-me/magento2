e881ec5fec720d39bff9bde8d5e6f38741923436
Maimona <maimona.hajadbullah@gmail.com>
2020-10-28 22:29:15 +0400

    Message fixed1

--- view/frontend/web/js/view/payment/method-renderer/spotiipay.js
+++ view/frontend/web/js/view/payment/method-renderer/spotiipay.js
@@ -450,7 +450,7 @@ define([
 
     isTotalValid: function () {
       var total = this.getGrandTotal() ? this.getGrandTotal() : window.checkoutConfig.quoteData.grand_total;
-      var curr = window.checkoutConfig.quoteData.base_currency_code;
+      var curr = window.checkoutConfig.quoteData.quote_currency_code;
       switch(curr){
         case "USD":
           total= total*3.6730;
