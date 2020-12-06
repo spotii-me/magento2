e544d9ede21b5a40b2b6dfb774c8264afd1604d2
Maimona <maimona.hajadbullah@gmail.com>
2020-12-06 11:25:00 +0400

    The order is not cancelled and the inventory is not changed

--- Controller/Standard/Cancel.php
+++ Controller/Standard/Cancel.php
@@ -28,26 +28,11 @@ class Cancel extends SpotiiPay
         $order = $this->_orderFactory->create()->loadByIncrementId($orderId);
         $paymentSubmitted = $this->getRequest()->getParam("submitted");
 
-        if($paymentSubmitted == '1'){
+        /*if($paymentSubmitted == '1'){
 
         $order->setState("canceled")->setStatus("canceled");
-        $order->save();
-        
-        foreach ($order->getAllVisibleItems() as $item) {
+        $order->save();*/
 
-            $sku = $item->getSku();
-            $qtyOrdered = $item->getQtyOrdered();
-
-            $stockItem = $this->stockRegistry->getStockItemBySku($sku);
-
-            $qtyInStock= $stockItem->getQty();
-            $finalQty = $qtyInStock +$qtyOrdered;
-
-            $stockItem->setQty($finalQty);
-            $stockItem->setIsInStock((bool)$finalQty);
-            $this->stockRegistry->updateStockItemBySku($sku, $stockItem);
-        }
-        
         $this->messageManager->addError("<b>Order Cancelled!</b><br> Your payment with Spotii cannot be completed as requested.");
         $order->registerCancellation("Returned from Spotii with completeing payment, order canceled.");
         $this->spotiiHelper->logSpotiiActions("Returned from Spotii without completeing payment, order canceled.");
@@ -55,21 +40,21 @@ class Cancel extends SpotiiPay
         $this->getResponse()->setRedirect(
             $this->_url->getUrl('checkout/onepage/failure')
         );
-    }else{
+    /*}else{
         $order->registerCancellation("Abandoned Cart");
         $this->spotiiHelper->logSpotiiActions("Abandoned Cart");
         $this->_checkoutSession->restoreQuote();
         $this->getResponse()->setRedirect(
             $this->_url->getUrl('checkout/onepage/failure') 
         );
-    }
+    }*/
      }catch (\Magento\Framework\Exception\LocalizedException $e) {
-        $this->spotiiHelper->logSpotiiActions("Redirect Exception: " . $e->getMessage());
+        $this->spotiiHelper->logSpotiiActions("Redirect Exception: cancel " . $e->getMessage());
         $this->messageManager->addError(
             $e->getMessage()
         );
       } catch (\Exception $e) {
-        $this->spotiiHelper->logSpotiiActions("Redirect Exception: " . $e->getMessage());
+        $this->spotiiHelper->logSpotiiActions("Redirect Exception: cancel " . $e->getMessage());
         $this->messageManager->addError(
             $e->getMessage()
         );
