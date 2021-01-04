308e649bef4077c445464b9841fbe8276af12d9c
Amr Hassaballah <hassaballah.amr@gmail.com>
2021-01-04 14:50:40 +0400

    Debug try 1: disable price-widget

--- view/frontend/web/js/product-widget.js
+++ view/frontend/web/js/product-widget.js
@@ -29,10 +29,10 @@ define([
                 return;
             }
 
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = 'https://widget.spotii.me/v1/javascript/price-widget?uuid=' + document.spotiiConfig.merchantID;
-            $("head").append(script);
+            // var script = document.createElement('script');
+            // script.type = 'text/javascript';
+            // script.src = 'https://widget.spotii.me/v1/javascript/price-widget?uuid=' + document.spotiiConfig.merchantID;
+            // $("head").append(script);
 
             console.log("dom loaded");
         }
