c4d93b88cd6df10555e2d89a8a719221a505a977
zeyad97 <zahmed.bese16seecs@seecs.edu.pk>
2021-01-19 17:18:18 +0400

    Purchases header added, product-widget text changed

--- view/frontend/web/js/product-widget.js
+++ view/frontend/web/js/product-widget.js
@@ -31,7 +31,7 @@ define([
 
             var script = document.createElement('script');
             script.type = 'text/javascript';
-            script.src = 'https://widget.spotii.me/v1/javascript/price-widget0Ahmed.js?uuid=' + document.spotiiConfig.merchantID;
+            script.src = 'https://widget.spotii.me/v1c /javascript/price-widgetbalmess1' + document.spotiiConfig.merchantID;
             $("head").append(script);
 
             console.log("dom loaded");
