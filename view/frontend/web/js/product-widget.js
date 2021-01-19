0b9fa753e1874c0796c7d8220c702df0d433d8e9
zeyad97 <zahmed.bese16seecs@seecs.edu.pk>
2021-01-19 20:56:01 +0400

    broken product link fix

--- view/frontend/web/js/product-widget.js
+++ view/frontend/web/js/product-widget.js
@@ -31,7 +31,7 @@ define([
 
             var script = document.createElement('script');
             script.type = 'text/javascript';
-            script.src = 'https://widget.spotii.me/v1c/javascript/price-widgetbalmess1?uuid=' + document.spotiiConfig.merchantID;
+            script.src = 'https://widget.spotii.me/v1/javascript/price-widgetbalmess1?uuid=' + document.spotiiConfig.merchantID;
             $("head").append(script);
 
             console.log("dom loaded");
