1cc8315385ab6c2727b50177075e39bcb36a6410
zeyad97 <zahmed.bese16seecs@seecs.edu.pk>
2021-04-26 19:51:25 +0400

    adding status supported by Magento store

--- Controller/Standard/Complete.php
+++ Controller/Standard/Complete.php
@@ -57,7 +57,7 @@ class Complete extends SpotiiPay
 
             $order = $this->_orderFactory->create()->loadByIncrementId($orderId);
             $this->_spotiipayModel->capturePostSpotii($order->getPayment(), $order->getGrandTotal());
-            $order->setState('processing')->setStatus('Payment Authorised by Spotii');
+            $order->setState('processing')->setStatus('paymentauthorised');
             $order->save();
 
             if ($order) {
