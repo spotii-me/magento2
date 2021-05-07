bb13f73a8e2a446b0b01bd3a60ee526f1a55fa25
zeyad97 <zahmed.bese16seecs@seecs.edu.pk>
2021-05-07 19:40:29 +0400

    enclosing in try block to catch error for loggedIn

--- Controller/Standard/Redirect.php
+++ Controller/Standard/Redirect.php
@@ -24,41 +24,45 @@ class Redirect extends SpotiiPay
      */
     public function execute()
     {
-        $this->spotiiHelper->logSpotiiActions("****Starting Spotii****");
-        $quote = $this->_checkoutSession->getQuote();
-        $this->spotiiHelper->logSpotiiActions("Quote Id : " . $quote->getId());
-        if ($this->_customerSession->isLoggedIn()) {
-            $customerId = $this->_customerSession->getCustomer()->getId();
-            $this->spotiiHelper->logSpotiiActions("Customer Id : $customerId");
-            $customer = $this->_customerRepository->getById($customerId);
-            $quote->setCustomer($customer);
-            $billingAddress = $quote->getBillingAddress();
-            $shippingAddress = $quote->getShippingAddress();
-            if ((empty($shippingAddress) || empty($shippingAddress->getStreetLine(1))) && (empty($billingAddress) || empty($billingAddress->getStreetLine(1)))) {
-                $json = $this->_jsonHelper->jsonEncode(["message" => "Please select an address"]);
-                $jsonResult = $this->_resultJsonFactory->create();
-                $jsonResult->setData($json);
-                return $jsonResult;
-            } elseif (empty($billingAddress) || empty($billingAddress->getStreetLine(1)) || empty($billingAddress->getFirstname())) {
-                $quote->setBillingAddress($shippingAddress);
+        try{
+            $this->spotiiHelper->logSpotiiActions("****Starting Spotii****");
+            $quote = $this->_checkoutSession->getQuote();
+            $this->spotiiHelper->logSpotiiActions("Quote Id : " . $quote->getId());
+            if ($this->_customerSession->isLoggedIn()) {
+                $customerId = $this->_customerSession->getCustomer()->getId();
+                $this->spotiiHelper->logSpotiiActions("Customer Id : $customerId");
+                $customer = $this->_customerRepository->getById($customerId);
+                $quote->setCustomer($customer);
+                $billingAddress = $quote->getBillingAddress();
+                $shippingAddress = $quote->getShippingAddress();
+                if ((empty($shippingAddress) || empty($shippingAddress->getStreetLine(1))) && (empty($billingAddress) || empty($billingAddress->getStreetLine(1)))) {
+                    $json = $this->_jsonHelper->jsonEncode(["message" => "Please select an address"]);
+                    $jsonResult = $this->_resultJsonFactory->create();
+                    $jsonResult->setData($json);
+                    return $jsonResult;
+                } elseif (empty($billingAddress) || empty($billingAddress->getStreetLine(1)) || empty($billingAddress->getFirstname())) {
+                    $quote->setBillingAddress($shippingAddress);
+                }
+            } else {
+                $post = $this->getRequest()->getPostValue();
+                $this->spotiiHelper->logSpotiiActions("Guest customer");
+                if (!empty($post['email'])) {
+                    $quote->setCustomerEmail($post['email'])
+                        ->setCustomerIsGuest(true)
+                        ->setCustomerGroupId(\Magento\Customer\Api\Data\GroupInterface::NOT_LOGGED_IN_ID);
+                }
+                $payment = $quote->getPayment();
+                $payment->setMethod('spotiipay');
+                $payment->save();
+                $quote->reserveOrderId();
+                $quote->setPayment($payment);
+                $quote->save();
+                $this->_checkoutSession->replaceQuote($quote);
+                $checkoutUrl = $this->_spotiipayModel->getSpotiiCheckoutUrl($quote);
+                $this->spotiiHelper->logSpotiiActions("Checkout Url : $checkoutUrl");
             }
-        } else {
-            $post = $this->getRequest()->getPostValue();
-            $this->spotiiHelper->logSpotiiActions("Guest customer");
-            if (!empty($post['email'])) {
-                $quote->setCustomerEmail($post['email'])
-                    ->setCustomerIsGuest(true)
-                    ->setCustomerGroupId(\Magento\Customer\Api\Data\GroupInterface::NOT_LOGGED_IN_ID);
-            }
-            $payment = $quote->getPayment();
-            $payment->setMethod('spotiipay');
-            $payment->save();
-            $quote->reserveOrderId();
-            $quote->setPayment($payment);
-            $quote->save();
-            $this->_checkoutSession->replaceQuote($quote);
-            $checkoutUrl = $this->_spotiipayModel->getSpotiiCheckoutUrl($quote);
-            $this->spotiiHelper->logSpotiiActions("Checkout Url : $checkoutUrl");
+        }catch(\Magento\Framework\Exception\LocalizedException $e){
+            $this->spotiiHelper->logSpotiiActions("Exception: " . $e->getMessage());
         }
         try{
         $json = $this->_jsonHelper->jsonEncode(["redirectURL" => $checkoutUrl]);
