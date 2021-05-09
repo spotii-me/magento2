424042110cf83fc5353143ffa33c8253c50e5bdb
Vignesh Vijay <35192681+Viggy9@users.noreply.github.com>
2021-05-09 22:07:19 +0400

    arrow fun for setTimeout

--- view/frontend/web/js/product-widget.js
+++ view/frontend/web/js/product-widget.js
@@ -3,39 +3,42 @@
  * @package     Spotii_Spotiipay
  * @copyright   Copyright (c) Spotii (https://www.spotii.me/)
  */
-define([
-    'jquery',
-    'ko',
-    'uiComponent',
-    'domReady!'
-], function ($, ko, Component) {
-    'use strict';
+define(['jquery', 'ko', 'uiComponent', 'domReady!'], function (
+	$,
+	ko,
+	Component
+) {
+	'use strict';
 
-    return Component.extend({
-        initialize: function () {
-            //initialize parent Component
-            this._super();
-            setTimeout(function(){ this.processSpotiiDocument()}, 10000);
-            // this.processSpotiiDocument();
-        },
+	return Component.extend({
+		initialize: function () {
+			//initialize parent Component
+			this._super();
+			setTimeout(() => this.processSpotiiDocument(), 10000);
+			// this.processSpotiiDocument();
+		},
 
-        processSpotiiDocument: function() {
-            console.log("rendering started");
-            var self = this;
-            console.log(self.jsConfig);
-            document.spotiiConfig = self.jsConfig;
+		processSpotiiDocument: function () {
+			console.log('rendering started');
+			var self = this;
+			console.log(self.jsConfig);
+			document.spotiiConfig = self.jsConfig;
 
-            if (!document.spotiiConfig) {
-                console.warn('SpotiiPay: document.spotiiConfig is not set, cannot render widget');
-                return;
-            }
+			if (!document.spotiiConfig) {
+				console.warn(
+					'SpotiiPay: document.spotiiConfig is not set, cannot render widget'
+				);
+				return;
+			}
 
-            var script = document.createElement('script');
-            script.type = 'text/javascript';
-            script.src = 'https://widget.spotii.me/v1/javascript/price-widget?uuid=' + document.spotiiConfig.merchantID;
-            $("head").append(script);
+			var script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.src =
+				'https://widget.spotii.me/v1/javascript/price-widget?uuid=' +
+				document.spotiiConfig.merchantID;
+			$('head').append(script);
 
-            console.log("dom loaded");
-        }
-    });
+			console.log('dom loaded');
+		}
+	});
 });
