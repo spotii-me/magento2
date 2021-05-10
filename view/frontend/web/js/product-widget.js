fcb4bdd20fff2c2b3567f6a4d0290e903e8d8a5e
Vignesh Vijay <35192681+Viggy9@users.noreply.github.com>
2021-05-10 23:09:56 +0400

    change in catalog

--- view/frontend/web/js/product-widget.js
+++ view/frontend/web/js/product-widget.js
@@ -11,79 +11,48 @@ define(['jquery', 'ko', 'uiComponent', 'domReady!'], function (
 	'use strict';
 
 	return Component.extend({
-		initialize: async function () {
+		initialize: function () {
 			//initialize parent Component
 			this._super();
-		 	this.loopThroughProduct();
+			this.processSpotiiDocument();
 		},
 
-		loopThroughProduct: function () {
+		processSpotiiDocument: function () {
+			console.log('rendering started');
+			// var self = this;
+			// console.log(self.jsConfig);
 			const allProducts = document.getElementsByClassName('price-wrapper');
+			console.log(allProducts, 'allProducts')
 			for (let product of allProducts) {
-				// console.log(this.jsConfig, 'js config in loop through product')
-				// this.jsConfig.renderToPath = ['#' + product.id];
-				// console.log(this.jsConfig.renderToPath, 'render Path in js config');
-				console.log(product.id, 'product ID tag');
 				var config = {
 					targetXPath: [".price-wrapper"],
 					renderToPath: ["#" + product.id],
 					currency: 'SAR'
 				}
-				this.processSpotiiDocument(config);
+				console.log(config, 'config');
+				// self.jsConfig.renderToPath = ["#"+product.id];
+				// console.log(self.jsConfig.renderToPath , 'render Path in js config')
+				document.spotiiConfig = config;
+				console.log(product.id, 'product ID tag');
+				console.log(document.spotiiConfig.renderToPath, 'render Path in spotii')
+				console.log(document.spotiiConfig, 'spotiiConfig')
+
+				if (!document.spotiiConfig) {
+					console.warn(
+						'SpotiiPay: document.spotiiConfig is not set, cannot render widget'
+					);
+					return;
+				}
+
+				var script = document.createElement('script');
+				script.type = 'text/javascript';
+				script.src =
+					'https://widget.spotii.me/v1/javascript/price-widget?uuid=' +
+					document.spotiiConfig.merchantID;
+				$('head').append(script);
+
+				console.log('dom loaded');
 			}
-		},
-
-		processSpotiiDocument: function (config) {
-			console.log(config, 'config')
-			console.log('rendering started');
-			// var self = this;
-			// console.log(self.jsConfig);
-			// const allProducts = document.getElementsByClassName('price-wrapper');
-			// console.log(allProducts, 'allProducts');
-			// for (let product of allProducts) {
-			// 	self.jsConfig.renderToPath = ['#' + product.id];
-			// 	console.log(self.jsConfig.renderToPath, 'render Path in js config');
-			// 	document.spotiiConfig = self.jsConfig;
-			// 	console.log(product.id, 'product ID tag');
-			// 	console.log(
-			// 		document.spotiiConfig.renderToPath,
-			// 		'render Path in spotii config'
-			// 	);
-			// 	console.log(document.spotiiConfig, 'spotiiConfig');
-
-			// 	if (!document.spotiiConfig) {
-			// 		console.warn(
-			// 			'SpotiiPay: document.spotiiConfig is not set, cannot render widget'
-			// 		);
-			// 		return;
-			// 	}
-
-			// 	var script = document.createElement('script');
-			// 	script.type = 'text/javascript';
-			// 	script.src =
-			// 		'https://widget.spotii.me/v1/javascript/price-widget?uuid=' +
-			// 		document.spotiiConfig.merchantID;
-			// 	$('head').append(script);
-
-			// 	console.log('dom loaded');
-			// }
-			document.spotiiConfig = config;
-			console.log(document.spotiiConfig.renderToPath,'render Path in spotii config');
-			console.log(document.spotiiConfig, 'spotiiConfig');
-
-			if (!document.spotiiConfig) {
-				console.warn(
-					'SpotiiPay: document.spotiiConfig is not set, cannot render widget'
-				);
-				return;
-			}
-
-			var script = document.createElement('script');
-			script.type = 'text/javascript';
-			script.src =
-				'https://widget.spotii.me/v1/javascript/price-widget'
-			$('head').append(script);
-			console.log('dom loaded');
-		},
+		}
 	});
 });
